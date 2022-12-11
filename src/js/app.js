7986d62aba17bd978aae87e456eefedf6ae3b1cb
David Haralambiev <114248239+CTSXT@users.noreply.github.com>
2022-12-12 01:43:10 +0200

    Update app.js

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,10 @@
 import "../scss/app.scss";
 
 window.addEventListener("DOMContentLoaded", () => {
+  const elements = document.querySelectorAll('.product');
+  const price=document.querySelector(".price").innerHTML
+  for(let i = 0 ; i<elements.length;i++){
+    elements[i].setAttribute("data-price",price);
+  }
   // This block will be executed once the page is loaded and ready
-
-
 });
